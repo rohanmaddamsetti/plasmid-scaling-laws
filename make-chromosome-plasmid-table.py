97c981629f258fe529a7389ceacdf7895c72ef07
Rohan Maddamsetti <rohan.maddamsetti@gmail.com>
2024-08-11 13:21:42 -0400

    updated chromosome-plasmid-table with taxonomy data

--- make-chromosome-plasmid-table.py
+++ make-chromosome-plasmid-table.py
@@ -10,10 +10,10 @@ from Bio import SeqIO
 
 warning_count = 0
 found_genomes = 0
-accessions_not_found_in_refseq = []
+accessions_not_found_on_disk = []
 
 with open("../results/chromosome-plasmid-table.csv",'w') as out_fh:
-    header = "AnnotationAccession,Organism,Strain,SeqID,SeqType,replicon_length\n"
+    header = "AnnotationAccession,Organism,Strain,TaxonomicGroup,TaxonomicSubgroup,SeqID,SeqType,replicon_length\n"
     out_fh.write(header)
     ## open the genome report file, and parse line by line.
     with open("../results/complete-prokaryotes-with-plasmids.txt", "r") as genome_report_fh:
@@ -24,18 +24,20 @@ with open("../results/chromosome-plasmid-table.csv",'w') as out_fh:
                 continue ## don't process the header any further.
             fields = line.split('\t')
             organism = fields[0]
+            taxon_group = fields[4]
+            taxon_subgroup = fields[5]
             strain = fields[-1]
             replicons = fields[8]
             ftp_path = fields[20]
-            annotation_accession = os.path.basename(ftp_path)
-            my_annotation_file = "../results/gbk-annotation/" + annotation_accession + "_genomic.gbff"
+            accession = os.path.basename(ftp_path)
+            my_annotation_file = "../results/gbk-annotation/" + accession + "_genomic.gbff"
             ''' make sure that this file exists in the annotation directory--
             skip if this was not the case.
             this is important; we don't want to include genomes that were
             not in the search database in the first place. '''
             if not os.path.exists(my_annotation_file):
                 warning_count += 1
-                accessions_not_found_in_refseq.append(annotation_accession)
+                accessions_not_found_on_disk.append(accession)
                 continue
             ## if we got here, then the RefSeq genbank annotation file was found.
             found_genomes += 1
@@ -53,16 +55,16 @@ with open("../results/chromosome-plasmid-table.csv",'w') as out_fh:
                         continue
                     replicon_length = str(len(replicon))
                     ## now write out the data for the replicon.
-                    row_data = [annotation_accession, organism, strain, SeqID, SeqType, replicon_length]
+                    row_data = [accession, organism, strain, taxon_group, taxon_subgroup, SeqID, SeqType, replicon_length]
                     ## replace all commas with semicolon to respect the csv output format.
                     row_string = ','.join([x.replace(',',';') for x in row_data]) + '\n'
                     out_fh.write(row_string)
 
 
 ## now print out warnings.
-print("Warning: the following accessions were not found in RefSeq. ")
-for annotation_accession in accessions_not_found_in_refseq:
-    print(annotation_accession)
-print("A total of " + str(warning_count) + " accessions were not found in RefSeq.")
-print("The most likely explanation is that these missing accessions did not pass RefSeq Quality Control.")
+print("Warning: the following accessions were not found on disk. ")
+for accession in accessions_not_found_on_disk:
+    print(accession)
+print("A total of " + str(warning_count) + " accessions were not found on disk.")
+print("The most likely explanations are that 1) they need to be downloaded. 2) they did not pass RefSeq Quality Control.")
 print("A total of " + str(found_genomes) + " genomes were found in RefSeq and processed.")
