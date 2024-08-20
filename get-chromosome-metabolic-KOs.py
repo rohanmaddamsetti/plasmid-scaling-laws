bf1e05b231b920f942a891283a910d74f3b7aa71
Rohan Maddamsetti <rohan.maddamsetti@gmail.com>
2024-08-20 01:48:47 -0400

    deleted cruft code

--- get-chromosome-metabolic-KOs.py
+++ get-chromosome-metabolic-KOs.py
@@ -16,39 +16,10 @@ for chromosome proteins with KOs in the list in
 """
 
 
-def get_replicons_from_genomes_with_chromosomes_smaller_than_a_plasmid(replicon_length_file):
-    bad_replicon_list  = []
-    cur_annotation_accession = None
-    cur_chromosome_length = -1
-    with open(replicon_length_file, "r") as replicon_length_fh:
-        for i, line in enumerate(replicon_length_fh):
-            if i == 0: continue ## skip the header
-            line = line.strip() ## remove leading and lagging whitespace.
-            AnnotationAccession, SeqID, SeqType, replicon_length, protein_count = line.split(",")
-            replicon_length = int(replicon_length)
-            protein_count = int(protein_count)
-            if AnnotationAccession != cur_annotation_accession and SeqType == "chromosome":
-                cur_annotation_accession = AnnotationAccession
-                cur_chromosome_length = replicon_length
-            else: ## AnnotationAccession == cur_annotation_accession
-                if (SeqType == "plasmid") and (replicon_length > cur_chromosome_length) and (SeqID not in bad_replicon_list):
-                    bad_replicon_list.append(SeqID)
-    return bad_replicon_list
-
-
 def main():
 
     """ IMPORTANT: This is a tsv file, because "," is a meaningful character in chemical names!   """
     replicon_length_file = "../results/replicon-lengths-and-protein-counts.csv"
-    """ make a list of genomes in which the chromosome is smaller than some plasmid.
-    These are often misannotated genomes in which the real chromosome is switched up with a plasmid."""
-    bad_replicon_list = get_replicons_from_genomes_with_chromosomes_smaller_than_a_plasmid(replicon_length_file)
-
-    print("*******")
-    print("BAD REPLICONS FROM GENOMES WITH A PLASMID APPARENTLY LARGER THAN THE CHROMOSOME:")
-    for x in bad_replicon_list:
-        print(x)
-    print("*******")
 
     ## for debugging other scripts: what is the set of SeqID prefixes?
     NCBI_prefixes = set()
@@ -68,10 +39,6 @@ def main():
             NCBI_prefix = SeqID.split("_")[0]
             NCBI_prefixes.add(NCBI_prefix)
 
-            ## skip over replicons in genomes in which there is a plasmid larger than the chromosome.
-            ## In some cases this is due to misannotation of chromosomes and plasmids in RefSeq genomes.
-            if SeqID in bad_replicon_list:
-                continue
             if KO_ID in metabolic_KEGG_IDs:
                 line_buffer.append("\t".join([AnnotationAccession, SeqID, SeqType, locus_tag, product]))
 
